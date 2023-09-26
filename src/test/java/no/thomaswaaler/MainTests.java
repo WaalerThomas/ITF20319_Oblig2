faab09ebc4c093b0577cb3a7ae2489980e4adb87
WaalerThomas <superxgenesis@gmail.com>
2023-09-26 13:28:45 +0200

    Initial setup of an empty project

---
+++ src/test/java/no/thomaswaaler/MainTests.java
@@ -0,0 +1,11 @@
+package no.thomaswaaler;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTests {
+    @Test
+    public void junitShouldFailThisTest() {
+        Assertions.assertFalse(true);
+    }
+}
