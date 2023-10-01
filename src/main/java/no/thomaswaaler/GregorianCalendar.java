efaf35459f581fc4fa4f4c53ec45a27cefd4fe2c
WaalerThomas <superxgenesis@gmail.com>
2023-10-01 15:56:03 +0200

    Finished?

--- src/main/java/no/thomaswaaler/GregorianCalendar.java
+++ src/main/java/no/thomaswaaler/GregorianCalendar.java
@@ -2,9 +2,10 @@ package no.thomaswaaler;
 
 public class GregorianCalendar {
     public static boolean isLeapYear(int year) {
-        if (year % 4 != 0)
-            return false;
+        if (year % 4 == 0 && year % 100 != 0
+        || year % 400 == 0)
+            return true;
 
-        return true;
+        return false;
     }
 }
--- src/test/java/no/thomaswaaler/Calendar_Tests.java
+++ src/test/java/no/thomaswaaler/Calendar_Tests.java
@@ -2,33 +2,70 @@ package no.thomaswaaler;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 /* Acceptance criteria:
- * [ ] Is leap year when
- *      [x] Year is divisible by 4
- *      [ ] Year is divisible by 4 and NOT 100
- *      [ ] Year is divisible by 400
+ * [x] Is leap year when
+ *      [x] Year is divisible by 4 and NOT 100
+ *      [x] Year is divisible by 400
  *
- * [ ] Is NOT leap year when
- *      [X] Year is NOT divisible by 4
- *      [ ] Year is divisible by 100
- *      [ ] Year is divisible by 100 and NOT 400
- *
- * [ ] Is leap year when year divisible by 4
- * [ ] Is NOT leap year when year NOT divisible by 4
- * [ ] Is NOT leap year when year divisible by 4 and 100
- * [ ] Is leap year when year divisible by 400
- * [ ] Is NOT leap year when year divisible by 100 and NOT 400
+ * [x] Is NOT leap year when
+ *      [x] Year is NOT divisible by 4
+ *      [x] Year is divisible by 100 and NOT 400
 */
 
 public class Calendar_Tests {
-    @Test
-    public void Leap_Year_96_Is_Divisible_By_4() {
-        Assertions.assertTrue(GregorianCalendar.isLeapYear(96));
+    @ParameterizedTest
+    @MethodSource("years_4_not_100")
+    public void Leap_Year_If_Divisible_By_4_And_Not_100(boolean expected, int input) {
+        Assertions.assertEquals(expected, GregorianCalendar.isLeapYear(input));
+    }
+
+    public static Stream years_4_not_100() {
+        return Stream.of(
+                Arguments.of(true, 1992),
+                Arguments.of(true, 1988),
+                Arguments.of(false, 1900),
+                Arguments.of(false, 1800)
+        );
     }
 
     @Test
-    public void Year_98_Is_Not_Divisible_By_4() {
-        Assertions.assertFalse(GregorianCalendar.isLeapYear(98));
+    public void Leap_Year_2000_Is_Divisible_By_400() {
+        Assertions.assertTrue(GregorianCalendar.isLeapYear(2000));
+    }
+
+    @ParameterizedTest
+    @MethodSource("years_not_4")
+    public void Not_Leap_Year_If_Not_Divisible_By_4(boolean expected, int input) {
+        Assertions.assertEquals(expected, GregorianCalendar.isLeapYear(input));
+    }
+
+    public static Stream years_not_4() {
+        return Stream.of(
+                Arguments.of(false, 1814),
+                Arguments.of(false, 1939),
+                Arguments.of(true, 1940),
+                Arguments.of(true, 1984)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("years_100_not_400")
+    public void Not_Leap_Year_If_Divisible_By_100_And_Not_400(boolean expected, int input) {
+        Assertions.assertEquals(expected, GregorianCalendar.isLeapYear(input));
+    }
+
+    public static Stream years_100_not_400() {
+        return Stream.of(
+                Arguments.of(false, 1700),
+                Arguments.of(false, 2100),
+                Arguments.of(true, 2400),
+                Arguments.of(true, 1600)
+        );
     }
 }
