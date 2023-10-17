ee503aaa24cd5323d1f1dafd2322611c8717b94d
TomWaa <superxgenesis@gmail.com>
2023-10-17 13:21:36 +0200

    Make some test fail

--- src/main/java/no/thomaswaaler/GregorianCalendar.java
+++ src/main/java/no/thomaswaaler/GregorianCalendar.java
@@ -2,7 +2,7 @@ package no.thomaswaaler;
 
 public class GregorianCalendar {
     public static boolean isLeapYear(int year) {
-        if (year % 4 == 0 && year % 100 != 0
+        if (year % 4 == 0 && year % 100 == 0
         || year % 400 == 0)
             return true;
 
